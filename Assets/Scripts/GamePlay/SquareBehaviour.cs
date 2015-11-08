4bf7732a27c0581981238bde39b4052a9702f496
eemmeeliiee <mimmi.emelie95@gmail.com>
2015-11-08 17:15:12 +0100

    Square Values
    We assigned the values to the edges of the square.

--- Assets/Scripts/GamePlay/SquareBehaviour.cs
+++ Assets/Scripts/GamePlay/SquareBehaviour.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class SquareBehaviour : MonoBehaviour {
 
     public bool isEnabled;
+
+    [Header("Side Values")]
+    public int[] sideValues = new int[4];
+    public Text[] sideValueText = new Text[4];
 
 	// Use this for initialization
 	void Start () {
@@ -23,5 +28,23 @@ public class SquareBehaviour : MonoBehaviour {
     {
         Vector3 _newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.localPosition = new Vector3(_newPosition.x, _newPosition.y, 0);
+    }
+
+    public void AssignValue(int value, int textID)
+    {
+        switch (value)
+        {
+            case (0):
+                sideValueText[textID].text = "N";
+                break;
+
+            case (1):
+                sideValueText[textID].text = "-";
+                break;
+
+            case (2):
+                sideValueText[textID].text = "+";
+                break;
+        }
     }
 }
---
+++ Assets/Scripts/Managers/SquareSpawnerManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SquareSpawnerManager : MonoBehaviour {
+
+    public GameObject square;
+    public int squaresToSpawn;
+
+	// Use this for initialization
+	void Start ()
+    {
+        SpawnSquares();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void SpawnSquares()
+    {
+        for (int i = 0; i < squaresToSpawn; i++)
+        {
+            GameObject newSquare = Instantiate(square, transform.position, transform.rotation) as GameObject;
+            SquareBehaviour squareScript = newSquare.GetComponent<SquareBehaviour>();
+
+            for (int j = 0; j < 4; j++)
+            {
+                squareScript.sideValues[j] = SideValue();
+                squareScript.AssignValue(squareScript.sideValues[j], j);
+            }
+        }
+    }
+
+    int SideValue()
+    {
+        int _value = Random.Range(0, 3);
+        return _value;
+    }
+
+}
